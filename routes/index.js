0234f001a02c3190b994e414c41f322356812e19
D'Arcey Taylor <darceytaylor05@gmail.com>
2016-02-25 13:44:21 -0500

    bootstrap/bower/navigation

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET Landing page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'My Favourite People' });
 });
+
+/* GET People page. */
+router.get('/people', function(req, res, next) {
+  res.render('index', { title: 'People Page' });
+});
+
+/* GET Contact page. */
+router.get('/contact', function(req, res, next) {
+  res.render('index', { title: 'Contact' });
+});
+
+
 
 module.exports = router;
